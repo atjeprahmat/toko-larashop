4a2d896db97b71bba41427a031c1f47469205c22
atjeprahmat <atjeprahmat@gmail.com>
2021-01-06 18:10:36 +0700

    first-sampai-create-user-baru

---
+++ database/seeders/AdministratorSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdministratorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $administrator = new \App\Models\User;
+        $administrator->username = "administrator";
+        $administrator->name = "Site Administrator";
+        $administrator->email = "administrator@larashop.test";
+        $administrator->roles = json_encode(["ADMIN"]);
+        $administrator->password = \Hash::make("larashop");
+        $administrator->avatar = "saat ini tidak ada file .png";
+        $administrator->address = "Bogor, Jawa Barat";
+
+        $administrator->save();
+
+        $this->command->info("User admin berhasil diinsert");
+    }
+}
